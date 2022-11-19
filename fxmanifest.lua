e9ebcbb086aed3f0889b7d787aee6886e8f5b7ec
LucaNL <lucanl.beheer@gmail.com>
2022-11-19 10:48:43 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'TimeAndDateDisplay-FiveM'
 author 'LucaNL#2230'
 description 'A script that allows you to always show the real life time and real life date on the top right of your screen.'
-version 'v2.0.0'
+version 'v2.0.1'
 url 'https://github.com/LucaNL/TimeAndDateDisplay-FiveM'
 
 server_script {
